d574a3165d797887106376488f81b88bc78de158
Jaime Forero-Romero <forero@users.noreply.github.com>
2018-06-18 13:54:48 -0500

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,8 +1,8 @@
 # Ejercicio 3 (20 puntos)
 
-A través de fotos aéreas se tienen medidas del alcance de un lanzador de proyectiles. 
-Se tienen cuatro valores (en metros): 61, 115, 31, 117. Todas las mediciones tienen una incertidumbre de 5 metros. 
-  El lanzador siempre da la misma velocidad inicial (entre 15 m/s y 60 m/s). 
-  Los valores diferentes del alcance se deben a diferentes ángulos de lanzamiento. 
-  Utilice métodos montecarlo y el teorema de Bayes para encontrar la velocidad más probable del lanzador de proyectiles 
-  a medida que se va incluyendo información nueva sobre los valores del alcance.
+# A través de fotos aéreas se tienen medidas del alcance de un lanzador de proyectiles. 
+# Se tienen cuatro valores (en metros): 61, 115, 31, 117. Todas las mediciones tienen una incertidumbre de 5 metros. 
+# El lanzador siempre da la misma velocidad inicial (entre 15 m/s y 60 m/s). 
+# Los valores diferentes del alcance se deben a diferentes ángulos de lanzamiento. 
+# Utilice métodos montecarlo y el teorema de Bayes para encontrar la velocidad más probable del lanzador de proyectiles 
+# a medida que se va incluyendo información nueva sobre los valores del alcance.
