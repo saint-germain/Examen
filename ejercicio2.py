dbffaac8533fa3b5ced85052c406a1c52afcfe05
saint-germain-examen-4crl76c3 <saint-germain-examen-4crl76c3@local>
2018-06-18 20:48:49 +0000

    2do ejercicio

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,3 +4,29 @@
 # La ecuación debe ser resuelta hasta un tiempo final T=0.5 y una velocidad c=-1.0.
 # El programa debe hacer una gráfica (adveccion.png) con la condición incial y el resultado final.
 
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+nx=1001
+xmin=-0.5
+xmax=0.5
+xdel=(xmax-xmin)/nx
+x=np.linspace(-0.5,0.5,nx)
+u0=np.exp(-x**2/0.1**2)
+c=-1
+nt=10001
+tmin=0
+tmax=0.5
+t=np.linspace(0,0.5,nt)
+tdel=(tmax-tmin)/nt
+u=u0
+for ti in t:
+    dxu=np.gradient(u)/xdel
+    u=u-tdel*c*dxu
+plt.plot(x,u,label='After')
+plt.plot(x,u0,label='Before')
+plt.legend()
+plt.xlabel('x')
+plt.ylabel('u')
+plt.savefig('adveccion.png')
