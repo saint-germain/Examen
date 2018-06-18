ae236013b39800c529eb5aa14f9abb1fb3e050a4
Jaime Forero-Romero <forero@users.noreply.github.com>
2018-06-18 13:58:52 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,6 @@
 # Ejercicio 2 (20 puntos)
-
+# Resuelva la ecuación de advección lineal d_t u + c d_x u = 0. 
+# La condición inicial es una función gaussiana centrada en 0.0 y de sigma 0.1.
+# La ecuación debe ser resuelta hasta un tiempo final T=0.5 y una velocidad c=-1.0.
+# El programa debe hacer una gráfica (adveccion.png) con la condición incial y el resultado final.
 
